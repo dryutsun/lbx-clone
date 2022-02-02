bf4202b9d5e5f77df07d2b397a833c9d7e775416
Goro <goro.ikeda.iyeki@gmail.com>
2022-02-02 13:55:10 -0500

    architecture readme added

---
+++ lbx-clone-msg/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <div className="navbar-container">
+
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
