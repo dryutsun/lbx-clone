7ddf7918038793d7c6860a9bd44bc950c27b0970
Goro <goro.ikeda.iyeki@gmail.com>
2022-02-16 16:16:27 -0500

    add navbar and mock user

--- lbx-clone-msg/src/App.js
+++ lbx-clone-msg/src/App.js
@@ -1,14 +1,26 @@
+// STYLESHEETS
 import "./App.css";
+// COMPONENTS
+import NavBar from "./components/NavBar";
+import Home from "./Home";
+// MODULES
+import { Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  // const [user, setUser] = useState("");
+  // setUser("Goro");
+  const user = "Goro"
+  // const user = null
   return (
-    <div className="App">
-      <nav>
-        <div className="navbar-container">
-
-        </div>
-      </nav>
-    </div>
+    <>
+      <div className="App">
+        <NavBar user={user} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- lbx-clone-msg/src/components/NavBar.js
+++ lbx-clone-msg/src/components/NavBar.js
@@ -1,8 +1,54 @@
-const NavBar = () => {
-    return (
-        <>
-        </>
-    )
-}
+import "../App.css";
+import { IoFilmSharp } from "react-icons/io5";
 
-export default NavBar
+// User Authenticated Route
+const authenticatedOptions = (
+  <>
+    <li id="nav-items">Profile</li>
+    <li id="nav-items">Films</li>
+    <li id="nav-items">Lists</li>
+    <li id="nav-items">Members</li>
+    <li id="nav-items">Journal</li>
+  </>
+);
+
+const unAuthenticatedOptions = (
+  <>
+    <li id="nav-items">Sign In</li>
+    <li id="nav-items">Create Account</li>
+    <li id="nav-items">Notebook</li>
+    <li id="nav-items">Lists</li>
+    <li id="nav-items">Reviews</li>
+  </>
+);
+
+const alwaysOptions = (
+  <>
+    <li id="nav-items">Home</li>
+  </>
+);
+
+const NavBar = ({ user }) => (
+    <>
+      <div className="navbar-container">
+        <div id="navbar-wrapper">
+          <div id="logo-icon">
+            <h1 id="logo">MOV.II</h1>
+            <IoFilmSharp style={{ color: "586067" }} />
+          </div>
+
+          <ul id="navbar-list">
+          {user && (
+					<span className='navbar-text mr-2'>Welcome, {user.email}</span>
+				)}
+            {alwaysOptions}
+            {user ? authenticatedOptions : unAuthenticatedOptions}
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+
+// Notes: This Logo-Icon Can be Turned into a Component
+// This Navbar Needs to be Conditional based Upon Aunthentication Route
+export default NavBar;
