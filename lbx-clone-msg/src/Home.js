2b6ccc20abe5d258f6e513eb589146e9e94adba8
Goro <goro.ikeda.iyeki@gmail.com>
2022-02-18 14:10:46 -0500

    add hero

--- lbx-clone-msg/src/Home.js
+++ lbx-clone-msg/src/Home.js
@@ -1,17 +1,27 @@
-import Hero from "./img/hero.gif"
+import Hero from "./img/hero.gif";
 
 const Home = () => {
-    return (
-        <>
-            <section>
-                <div id="hero-wrapper">
-                    <div id="hero-image">
-                        <img id="hero" src={Hero} />
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="page-wrapper">
+        <div id="hero-image-wrapper">
+          <div id="hero-image-blur">
+            <img id="hero" src={Hero} />
+          </div>
+          <section id="title-text">
+            <h1 className="HeroText">Track Films You've Watched.</h1>
+            <h1 className="HeroText">Save Ones You Want To See.</h1>
+            <h1 className="HeroText">Tell your friends what's good.</h1>
+          </section>
+          <div id="content-wrapper"></div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+// TODO:
+// - Find a way to mimic the "border blur in letterboxd"
+// - Do I need to create a Component Page for the "Content Page"?
+
+export default Home;
--- lbx-clone-msg/src/components/Auth/Sign-Up.js
+++ lbx-clone-msg/src/components/Auth/Sign-Up.js
@@ -1,5 +1,6 @@
 const SignIn = () => {
-    return (<>
+    return (
+    <>
     </>)
 }
 
